31984e84c9d83fcc994df4ed50e1805384308c56
Manish Kapoor <manish.kapoor@airfi.aero>
2018-02-03 11:35:17 +0530

    How to specify join table in many to many relationships:     In Book     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),            inverseJoinColumns = @JoinColumn(name = "author_id"))
        In Author:
        @ManyToMany(mappedBy = "authors")

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,6 +21,8 @@ public class Book {
     private String publisher;
 
     @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
